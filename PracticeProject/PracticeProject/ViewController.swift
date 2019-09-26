e0b33f4dafbcc289b87b990446f5d7c4a0ef8bf6
Hector Morales veloz <hector@Hectors-MacBook-Pro.local>
2019-09-26 10:19:06 -0400

    Create starter project

---
+++ PracticeProject/PracticeProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticeProject
+//
+//  Created by Hector Morales veloz on 9/26/19.
+//  Copyright © 2019 Hector Morales veloz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
